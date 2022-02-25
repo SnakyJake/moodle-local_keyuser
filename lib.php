ad57567e0c05af0f005b1693d095f7d1960ea2bd
Jakob Heinemann <jakob@jakobheinemann.de>
2022-02-25 16:33:24 +0100

    adding profile edit button to profile

--- lib.php
+++ lib.php
@@ -27,6 +27,7 @@ defined('MOODLE_INTERNAL') || die;
 
 require_once($CFG->dirroot.'/cohort/lib.php');
 require_once($CFG->libdir . '/formslib.php');
+require_once('locallib.php');
 
 MoodleQuickForm::registerElementType('keyusercohort', "$CFG->dirroot/local/keyuser/classes/form/keyusercohort.php", 'MoodleQuickForm_keyusercohort');
 
@@ -59,4 +60,35 @@ function local_keyuser_inplace_editable($itemtype, $itemid, $newvalue) {
     if ($itemtype === 'cohortname') {
         return \local_keyuser\output\cohortname::update($itemid, $newvalue);
     }
+}
+
+function local_keyuser_myprofile_navigation($tree, $user, $iscurrentuser, $course){
+    global $USER;
+    $systemcontext = context_system::instance();
+    $courseid = !empty($course) ? $course->id : SITEID;
+
+    if (($iscurrentuser || is_siteadmin($USER) || !is_siteadmin($user)) && has_capability('local/keyuser:userupdate', $systemcontext)) {
+        $url = new moodle_url('/user/editadvanced.php', array('id' => $user->id, 'course' => $courseid,
+            'returnto' => 'profile'));
+        $node = new core_user\output\myprofile\node('contact', 'editprofile', get_string('editmyprofile'), null, $url,
+            null, null, 'editprofile');
+        $tree->add_node($node);
+    }
+}
+
+function local_keyuser_control_view_profile($user, $course = null, context_user $usercontext = null) {
+    global $DB,$PAGE;
+    $systemcontext = context_system::instance();
+    if(has_capability("local/keyuser:userupdate",$systemcontext)){
+        $sql = "SELECT count(*) as count FROM {user} ";
+        $wheresql = "WHERE {user}.id = :id";
+        $params['id'] = $user->id;
+        keyuser_user_append_where($wheresql,$params);
+        $r = $DB->get_record_sql($sql.$wheresql, $params);
+        if($r->count > 0){
+            //$PAGE->set_other_editing_capability("local/keyuser:userupdate");         
+            return core_user::VIEWPROFILE_FORCE_ALLOW;
+        }
+    }
+    return core_user::VIEWPROFILE_DO_NOT_PREVENT;
 }
