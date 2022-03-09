dddf965bddfb77d47a67afeaef97a076527acf66
Jakob Heinemann <jakob@jakobheinemann.de>
2022-03-09 13:53:16 +0100

    version bump

--- version.php
+++ version.php
@@ -26,7 +26,7 @@
 defined('MOODLE_INTERNAL') || die;
 
 $plugin->component = 'local_keyuser';
-$plugin->version  = 2022030900;
-$plugin->release = "1.0.0 ($plugin->version)";
+$plugin->version  = 2022030901;
+$plugin->release = "1.0.1 ($plugin->version)";
 $plugin->requires = 2021051700;
 $plugin->maturity = MATURITY_STABLE;
