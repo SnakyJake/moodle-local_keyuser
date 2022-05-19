88cff818c620d08c62b74daff815e95ac22654ff
fbech <f.bech@koppelsberg.de>
2022-05-19 11:32:41 +0200

    Added SELECT_KEYUSER_COHORT and FROM_KEYUSER_COHORT in cohort/lib.php FROM_KEYUSER_COHORT replaces " FROM {cohort} c" in SQL-statements

--- cohort/lib.php
+++ cohort/lib.php
@@ -24,6 +24,11 @@
 
 defined('MOODLE_INTERNAL') || die();
 
+define('SELECT_KEYUSER_COHORT', "SELECT c.id, c.contextid, SUBSTRING(c.idnumber, LENGTH(c.prefix)+1) as name, SUBSTRING(c.idnumber, LENGTH(c.prefix)+1) as idnumber, c.description, c.descriptionformat, c.visible, c.component, c.timecreated, c.timemodified, c.theme, c.name as realname, c.idnumber as realidnumber, INSTR(c.prefix, '_r_') > 0 as readonly");
+define('FROM_KEYUSER_COHORT',	 " FROM (SELECT *, REGEXP_SUBSTR(idnumber, :prefix) as prefix
+                                           FROM {cohort}
+                                         HAVING prefix IS NOT NULL) c");
+
 require_once($CFG->dirroot . '/cohort/lib.php');
 
 /**
@@ -62,14 +67,10 @@ function keyuser_cohort_update_cohort($cohort) {
 function keyuser_cohort_get_record($id) {
     global $DB;
 
-    $fields = "SELECT id, contextid, SUBSTRING(idnumber, LENGTH(prefix)+1) as name, SUBSTRING(idnumber, LENGTH(prefix)+1) as idnumber, description, descriptionformat, visible, component, timecreated, timemodified, theme, name as realname, idnumber as realidnumber, INSTR(prefix, '_r_') > 0 as readonly";
-    $sql = " FROM (SELECT *, REGEXP_SUBSTR(idnumber, :prefix) as prefix
-                     FROM {cohort}
-                    WHERE id = :id
-                   HAVING prefix IS NOT NULL) c";
-    $params = array('id' => $id, 'prefix' => keyuser_cohort_get_prefix_regexp());
+    $sql = " WHERE id = :id";
+    $params = array('prefix' => keyuser_cohort_get_prefix_regexp(), 'id' => $id);
 
-    return $DB->get_record_sql($fields . $sql, $params, MUST_EXIST);
+    return $DB->get_record_sql(SELECT_KEYUSER_COHORT . FROM_KEYUSER_COHORT . $sql, $params, MUST_EXIST);
 }
 
 /**
@@ -80,14 +81,10 @@ function keyuser_cohort_get_record($id) {
 function keyuser_cohort_get_records() {
     global $DB;
 
-    $fields = "SELECT id, contextid, SUBSTRING(idnumber, LENGTH(prefix)+1) as name, SUBSTRING(idnumber, LENGTH(prefix)+1) as idnumber, description, descriptionformat, visible, component, timecreated, timemodified, theme, name as realname, idnumber as realidnumber, INSTR(prefix, '_r_') > 0 as readonly";
-    $sql = " FROM (SELECT *, REGEXP_SUBSTR(idnumber, :prefix) as prefix
-                     FROM {cohort}
-                   HAVING prefix IS NOT NULL) c";
     $params = array('prefix' => keyuser_cohort_get_prefix_regexp());
-    $order = " ORDER BY name ASC";
+    $order = " ORDER BY name ASC, idnumber ASC";
 
-    return $DB->get_records_sql($fields . $sql . $order, $params);
+    return $DB->get_records_sql(SELECT_KEYUSER_COHORT . FROM_KEYUSER_COHORT . $order, $params);
 }
 
 /**
@@ -117,24 +114,15 @@ function keyuser_cohort_record_exists($idnumber) {
  */
 function keyuser_cohort_get_cohorts($contextid, $page = 0, $perpage = 25, $search = '') {
     global $DB;
-/*
-    $columns = $DB->get_columns('cohort');
-    unset($columns['name']);
-    unset($columns['idnumber']);
-    $str = implode(", ", array_keys($columns));
-    print($str);
-*/
-    $fields = "SELECT id, contextid, SUBSTRING(idnumber, LENGTH(prefix)+1) as name, SUBSTRING(idnumber, LENGTH(prefix)+1) as idnumber, description, descriptionformat, visible, component, timecreated, timemodified, theme, name as realname, idnumber as realidnumber, INSTR(prefix, '_r_') > 0 as readonly";
+
+    $fields = SELECT_KEYUSER_COHORT;
     $countfields = "SELECT COUNT(1)";
-    $sql = " FROM (SELECT *, REGEXP_SUBSTR(idnumber, :prefix) as prefix
-                     FROM {cohort}
-                    WHERE contextid = :contextid";
-    $having = " HAVING prefix IS NOT NULL) c";
+    $sql =   FROM_KEYUSER_COHORT."
+             WHERE contextid = :contextid";
     $params = array('prefix' => keyuser_cohort_get_prefix_regexp(), 'contextid' => $contextid);
     $order = " ORDER BY name ASC, idnumber ASC";
 
-    $totalcohorts = $allcohorts = $DB->count_records_sql($countfields . $sql . $having, $params);
-
+    $totalcohorts = $allcohorts = $DB->count_records_sql($countfields . $sql, $params);
     if (!empty($search)) {
         list($searchcondition, $searchparams) = cohort_get_search_query($search);
         $sql .= ' AND ' . $searchcondition;
@@ -142,9 +130,9 @@ function keyuser_cohort_get_cohorts($contextid, $page = 0, $perpage = 25, $searc
     }
 
     if (!empty($search)) {
-        $totalcohorts = $DB->count_records_sql($countfields . $sql . $having, $params);
+        $totalcohorts = $DB->count_records_sql($countfields . $sql, $params);
     }
-    $cohorts = $DB->get_records_sql($fields . $sql . $having . $order, $params, $page*$perpage, $perpage);
+    $cohorts = $DB->get_records_sql($fields . $sql . $order, $params, $page*$perpage, $perpage);
 
     return array('totalcohorts' => $totalcohorts, 'cohorts' => $cohorts, 'allcohorts' => $allcohorts);
 }
@@ -164,12 +152,10 @@ function keyuser_cohort_get_cohorts($contextid, $page = 0, $perpage = 25, $searc
 function keyuser_cohort_get_all_cohorts($page = 0, $perpage = 25, $search = '') {
     global $DB;
 
-    $fields = "SELECT id, contextid, SUBSTRING(idnumber, LENGTH(prefix)+1) as name, SUBSTRING(idnumber, LENGTH(prefix)+1) as idnumber, description, descriptionformat, visible, component, timecreated, timemodified, theme, name as realname, idnumber as realidnumber, INSTR(prefix, '_r_') > 0 as readonly, ".context_helper::get_preload_record_columns_sql('ctx');
+    $fields = SELECT_KEYUSER_COHORT.", ".context_helper::get_preload_record_columns_sql('ctx');
     $countfields = "SELECT COUNT(*)";
-    $sql = " FROM (SELECT *, REGEXP_SUBSTR(idnumber, :prefix) as prefix
-                     FROM {cohort}";
-    $having = " HAVING prefix IS NOT NULL) c";
-    $join = " JOIN {context} ctx ON ctx.id = c.contextid ";
+    $sql =   FROM_KEYUSER_COHORT."
+             JOIN {context} ctx ON ctx.id = c.contextid ";
     $params = array('prefix' => keyuser_cohort_get_prefix_regexp());
     $wheresql = '';
 
@@ -179,17 +165,17 @@ function keyuser_cohort_get_all_cohorts($page = 0, $perpage = 25, $search = '') 
         $params = array_merge($params, $excludedparams);
     }
 
-    $totalcohorts = $allcohorts = $DB->count_records_sql($countfields . $sql . $wheresql . $having . $join, $params);
+    $totalcohorts = $allcohorts = $DB->count_records_sql($countfields . $sql . $wheresql, $params);
 
     if (!empty($search)) {
         list($searchcondition, $searchparams) = cohort_get_search_query($search, 'c');
         $wheresql .= ($wheresql ? ' AND ' : ' WHERE ') . $searchcondition;
         $params = array_merge($params, $searchparams);
-        $totalcohorts = $DB->count_records_sql($countfields . $sql . $wheresql . $having . $join, $params);
+        $totalcohorts = $DB->count_records_sql($countfields . $sql . $wheresql, $params);
     }
 
-    $order = " ORDER BY suffix ASC";
-    $cohorts = $DB->get_records_sql($fields . $sql . $wheresql . $having . $join . $order, $params, $page*$perpage, $perpage);
+    $order = " ORDER BY c.name ASC, c.idnumber ASC";
+    $cohorts = $DB->get_records_sql($fields . $sql . $wheresql . $order, $params, $page*$perpage, $perpage);
 
     // Preload used contexts, they will be used to check view/manage/assign capabilities and display categories names.
     foreach (array_keys($cohorts) as $key) {
@@ -216,13 +202,13 @@ function keyuser_cohort_get_all_cohorts($page = 0, $perpage = 25, $search = '') 
 function keyuser_cohort_get_available_cohorts($currentcontext, $withmembers = 0, $offset = 0, $limit = 25, $search = '') {
     global $DB;
 
-    $params = array();
+    $params = array('prefix' => keyuser_cohort_get_prefix_regexp());
 
     // Build context subquery. Find the list of parent context where user is able to see any or visible-only cohorts.
     // Since this method is normally called for the current course all parent contexts are already preloaded.
     $contextsany = array_filter($currentcontext->get_parent_context_ids(),
         function($a) {
-            return has_capability("local/keyuser:cohortview", context::instance_by_id($a));
+            return has_capability("moodle/cohort:view", context::instance_by_id($a));
         });
     $contextsvisible = array_diff($currentcontext->get_parent_context_ids(), $contextsany);
     if (empty($contextsany) && empty($contextsvisible)) {
@@ -242,16 +228,13 @@ function keyuser_cohort_get_available_cohorts($currentcontext, $withmembers = 0,
     }
     $wheresql = '(' . implode(' OR ', $subqueries) . ')';
 
-    keyuser_cohort_append_where($wheresql,$params);
-
     // Build the rest of the query.
     $fromsql = "";
-    $fieldssql = 'c.id, c.name, c.contextid, c.idnumber, c.visible';
+    $fields = SELECT_KEYUSER_COHORT;
     $groupbysql = '';
     $havingsql = '';
     if ($withmembers) {
-        $fieldssql .= ', s.memberscnt';
-        $subfields = "c.id, COUNT(DISTINCT cm.userid) AS memberscnt";
+        $fields .= "COUNT(DISTINCT cm.userid) AS memberscnt";
         $groupbysql = " GROUP BY c.id";
         $fromsql = " LEFT JOIN {cohort_members} cm ON cm.cohortid = c.id ";
         if (in_array($withmembers,
@@ -259,8 +242,7 @@ function keyuser_cohort_get_available_cohorts($currentcontext, $withmembers = 0,
             list($esql, $params2) = get_enrolled_sql($currentcontext);
             $fromsql .= " LEFT JOIN ($esql) u ON u.id = cm.userid ";
             $params = array_merge($params2, $params);
-            $fieldssql .= ', s.enrolledcnt';
-            $subfields .= ', COUNT(DISTINCT u.id) AS enrolledcnt';
+            $fields .= ', COUNT(DISTINCT u.id) AS enrolledcnt';
         }
         if ($withmembers == COHORT_WITH_MEMBERS_ONLY) {
             $havingsql = " HAVING COUNT(DISTINCT cm.userid) > 0";
@@ -276,24 +258,13 @@ function keyuser_cohort_get_available_cohorts($currentcontext, $withmembers = 0,
         $params = array_merge($params, $searchparams);
     }
 
-    $prefixlen = strlen(keyuser_cohort_get_prefix()) + 1;
-    if ($withmembers) {
-        $sql = "SELECT " . str_replace('c.', 'cohort.', $fieldssql) . "
-                  FROM {cohort} cohort
-                  JOIN (SELECT $subfields
-                          FROM {cohort} c $fromsql
-                         WHERE $wheresql $groupbysql $havingsql
-                        ) s ON cohort.id = s.id";
-        $sql .= " ORDER BY SUBSTRING(cohort.name,".$prefixlen."+(INSTR(SUBSTRING(cohort.name,".$prefixlen.",2),'r_')*2))";
-    } else {
-        $sql = "SELECT $fieldssql
-                  FROM {cohort} c $fromsql
-                 WHERE $wheresql";
-        $sql .= " ORDER BY SUBSTRING(c.name,".$prefixlen."+(INSTR(SUBSTRING(c.name,".$prefixlen.",2),'r_')*2))";
-    }
+    $sql = $fields .
+           FROM_KEYUSER_COHORT . $fromsql .
+        " WHERE $wheresql $groupbysql $havingsql
+       ORDER BY name, idnumber";
 
-
-    return $DB->get_records_sql($sql, $params, $offset, $limit);
+    $return = $DB->get_records_sql($sql, $params, $offset, $limit);
+    return $return;
 }
 
 // cohort_get_cohort() and cohort_get_user_cohorts() need no implementation
@@ -315,13 +286,12 @@ function keyuser_cohort_edit_controls(context $context, moodle_url $currenturl) 
     }
     if ($context->contextlevel == CONTEXT_SYSTEM) {
         $tabs[] = new tabobject('view', new moodle_url($viewurl, array('showall' => 0)), get_string('systemcohorts', 'cohort'));
-        /* limit keyuser to system cohorts for now
-         *
-         * $tabs[] = new tabobject('viewall', new moodle_url($viewurl, array('showall' => 1)), get_string('allcohorts', 'cohort'));
-         * if ($currenturl->get_param('showall')) {
-         *     $currenttab = 'viewall';
-         * }
-         */
+/*limit keyuser to system cohorts for now
+        $tabs[] = new tabobject('viewall', new moodle_url($viewurl, array('showall' => 1)), get_string('allcohorts', 'cohort'));
+        if ($currenturl->get_param('showall')) {
+            $currenttab = 'viewall';
+        }
+*/
     } else {
         $tabs[] = new tabobject('view', $viewurl, get_string('cohorts', 'cohort'));
     }
