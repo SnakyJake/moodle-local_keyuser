54759b6a0baf10fb77ac9aaafeb49a4fbd5e5c45
fbech <f.bech@koppelsberg.de>
2022-05-19 12:57:14 +0200

    cohort/lib.php clean up for shorter diff diff -u orgmoodlefiles/cohort/lib.php cohort/lib.php

--- cohort/lib.php
+++ cohort/lib.php
@@ -24,12 +24,13 @@
 
 defined('MOODLE_INTERNAL') || die();
 
+require_once($CFG->dirroot . '/cohort/lib.php');
+require_once($CFG->dirroot.'/local/keyuser/locallib.php');
+
 define('SELECT_KEYUSER_COHORT', "SELECT c.id, c.contextid, SUBSTRING(c.idnumber, LENGTH(c.prefix)+1) as name, SUBSTRING(c.idnumber, LENGTH(c.prefix)+1) as idnumber, c.description, c.descriptionformat, c.visible, c.component, c.timecreated, c.timemodified, c.theme, c.name as realname, c.idnumber as realidnumber, INSTR(c.prefix, '_r_') > 0 as readonly");
 define('FROM_KEYUSER_COHORT',	 " FROM (SELECT *, REGEXP_SUBSTR(idnumber, :prefix) as prefix
                                            FROM {cohort}
                                          HAVING prefix IS NOT NULL) c");
-
-require_once($CFG->dirroot . '/cohort/lib.php');
 
 /**
  * Add new keyuser_cohort.
@@ -38,24 +39,161 @@ require_once($CFG->dirroot . '/cohort/lib.php');
  * @return int new cohort id
  */
 function keyuser_cohort_add_cohort($cohort) {
-    if (keyuser_cohort_add_prefix($cohort->name)) {
-        $cohort->idnumber = $cohort->name;
-        return cohort_add_cohort($cohort);
+    global $DB, $CFG;
+
+    if (!isset($cohort->name)) {
+        throw new coding_exception('Missing cohort name in cohort_add_cohort().');
     }
-    return -1;
+    if (!isset($cohort->idnumber)) {
+        $cohort->idnumber = NULL;
+    }
+    if (!isset($cohort->description)) {
+        $cohort->description = '';
+    }
+    if (!isset($cohort->descriptionformat)) {
+        $cohort->descriptionformat = FORMAT_HTML;
+    }
+    if (!isset($cohort->visible)) {
+        $cohort->visible = 1;
+    }
+    if (empty($cohort->component)) {
+        $cohort->component = '';
+    }
+    if (empty($CFG->allowcohortthemes) && isset($cohort->theme)) {
+        unset($cohort->theme);
+    }
+    if (empty($cohort->theme) || empty($CFG->allowcohortthemes)) {
+        $cohort->theme = '';
+    }
+    if (!isset($cohort->timecreated)) {
+        $cohort->timecreated = time();
+    }
+    if (!isset($cohort->timemodified)) {
+        $cohort->timemodified = $cohort->timecreated;
+    }
+
+    if (!keyuser_cohort_add_prefix($cohort->name))
+        return 0;
+    $cohort->idnumber = $cohort->name;
+    $cohort->id = $DB->insert_record('cohort', $cohort);
+
+    $event = \core\event\cohort_created::create(array(
+        'context' => context::instance_by_id($cohort->contextid),
+        'objectid' => $cohort->id,
+    ));
+    $event->add_record_snapshot('cohort', $cohort);
+    $event->trigger();
+
+    return $cohort->id;
 }
 
 /**
  * Update existing keyuser_cohort.
- *
  * @param  stdClass $cohort
  * @return void
  */
 function keyuser_cohort_update_cohort($cohort) {
-    if (keyuser_cohort_add_prefix($cohort->name)) {
-        $cohort->idnumber = $cohort->name;
-        return cohort_update_cohort($cohort);
+    global $DB, $CFG;
+    if (property_exists($cohort, 'component') and empty($cohort->component)) {
+        // prevent NULLs
+        $cohort->component = '';
     }
+    // Only unset the cohort theme if allowcohortthemes is enabled to prevent the value from being overwritten.
+    if (empty($CFG->allowcohortthemes) && isset($cohort->theme)) {
+        unset($cohort->theme);
+    }
+    $cohort->timemodified = time();
+    if (!keyuser_cohort_add_prefix($cohort->name))
+        return;
+    $cohort->idnumber = $cohort->name;
+    $DB->update_record('cohort', $cohort);
+
+    $event = \core\event\cohort_updated::create(array(
+        'context' => context::instance_by_id($cohort->contextid),
+        'objectid' => $cohort->id,
+    ));
+    $event->trigger();
+}
+
+/**
+ * Returns the list of keyuser_cohorts visible to the current user in the given course.
+ *
+ * The following fields are returned in each record: id, name, contextid, idnumber, visible
+ * Fields memberscnt and enrolledcnt will be also returned if requested
+ *
+ * @param context $currentcontext
+ * @param int $withmembers one of the COHORT_XXX constants that allows to return non empty cohorts only
+ *      or cohorts with enroled/not enroled users, or just return members count
+ * @param int $offset
+ * @param int $limit
+ * @param string $search
+ * @return array
+ */
+function keyuser_cohort_get_available_cohorts($currentcontext, $withmembers = 0, $offset = 0, $limit = 25, $search = '') {
+    global $DB;
+
+    $params = array('prefix' => keyuser_cohort_get_prefix_regexp());
+
+    // Build context subquery. Find the list of parent context where user is able to see any or visible-only cohorts.
+    // Since this method is normally called for the current course all parent contexts are already preloaded.
+    $contextsany = array_filter($currentcontext->get_parent_context_ids(),
+        function($a) {
+            return has_capability("moodle/cohort:view", context::instance_by_id($a));
+        });
+    $contextsvisible = array_diff($currentcontext->get_parent_context_ids(), $contextsany);
+    if (empty($contextsany) && empty($contextsvisible)) {
+        // User does not have any permissions to view cohorts.
+        return array();
+    }
+    $subqueries = array();
+    if (!empty($contextsany)) {
+        list($parentsql, $params1) = $DB->get_in_or_equal($contextsany, SQL_PARAMS_NAMED, 'ctxa');
+        $subqueries[] = 'c.contextid ' . $parentsql;
+        $params = array_merge($params, $params1);
+    }
+    if (!empty($contextsvisible)) {
+        list($parentsql, $params1) = $DB->get_in_or_equal($contextsvisible, SQL_PARAMS_NAMED, 'ctxv');
+        $subqueries[] = '(c.visible = 1 AND c.contextid ' . $parentsql. ')';
+        $params = array_merge($params, $params1);
+    }
+    $wheresql = '(' . implode(' OR ', $subqueries) . ')';
+
+    // Build the rest of the query.
+    $fromsql = "";
+    $fields = SELECT_KEYUSER_COHORT;
+    $groupbysql = '';
+    $havingsql = '';
+    if ($withmembers) {
+        $fields .= "COUNT(DISTINCT cm.userid) AS memberscnt";
+        $groupbysql = " GROUP BY c.id";
+        $fromsql = " LEFT JOIN {cohort_members} cm ON cm.cohortid = c.id ";
+        if (in_array($withmembers,
+                array(COHORT_COUNT_ENROLLED_MEMBERS, COHORT_WITH_ENROLLED_MEMBERS_ONLY, COHORT_WITH_NOTENROLLED_MEMBERS_ONLY))) {
+            list($esql, $params2) = get_enrolled_sql($currentcontext);
+            $fromsql .= " LEFT JOIN ($esql) u ON u.id = cm.userid ";
+            $params = array_merge($params2, $params);
+            $fields .= ', COUNT(DISTINCT u.id) AS enrolledcnt';
+        }
+        if ($withmembers == COHORT_WITH_MEMBERS_ONLY) {
+            $havingsql = " HAVING COUNT(DISTINCT cm.userid) > 0";
+        } else if ($withmembers == COHORT_WITH_ENROLLED_MEMBERS_ONLY) {
+            $havingsql = " HAVING COUNT(DISTINCT u.id) > 0";
+        } else if ($withmembers == COHORT_WITH_NOTENROLLED_MEMBERS_ONLY) {
+            $havingsql = " HAVING COUNT(DISTINCT cm.userid) > COUNT(DISTINCT u.id)";
+        }
+    }
+    if ($search) {
+        list($searchsql, $searchparams) = cohort_get_search_query($search);
+        $wheresql .= ' AND ' . $searchsql;
+        $params = array_merge($params, $searchparams);
+    }
+
+    $sql = $fields .
+              FROM_KEYUSER_COHORT . $fromsql .
+           " WHERE $wheresql $groupbysql $havingsql
+          ORDER BY name, idnumber";
+
+    return $DB->get_records_sql($sql, $params, $offset, $limit);
 }
 
 /**
@@ -142,91 +280,6 @@ function keyuser_cohort_get_all_cohorts($page = 0, $perpage = 25, $search = '') 
 
     return array('totalcohorts' => $totalcohorts, 'cohorts' => $cohorts, 'allcohorts' => $allcohorts);
 }
-
-/**
- * Returns the list of keyuser_cohorts visible to the current user in the given course.
- *
- * The following fields are returned in each record: id, name, contextid, idnumber, visible
- * Fields memberscnt and enrolledcnt will be also returned if requested
- *
- * @param context $currentcontext
- * @param int $withmembers one of the COHORT_XXX constants that allows to return non empty cohorts only
- *      or cohorts with enroled/not enroled users, or just return members count
- * @param int $offset
- * @param int $limit
- * @param string $search
- * @return array
- */
-function keyuser_cohort_get_available_cohorts($currentcontext, $withmembers = 0, $offset = 0, $limit = 25, $search = '') {
-    global $DB;
-
-    $params = array('prefix' => keyuser_cohort_get_prefix_regexp());
-
-    // Build context subquery. Find the list of parent context where user is able to see any or visible-only cohorts.
-    // Since this method is normally called for the current course all parent contexts are already preloaded.
-    $contextsany = array_filter($currentcontext->get_parent_context_ids(),
-        function($a) {
-            return has_capability("moodle/cohort:view", context::instance_by_id($a));
-        });
-    $contextsvisible = array_diff($currentcontext->get_parent_context_ids(), $contextsany);
-    if (empty($contextsany) && empty($contextsvisible)) {
-        // User does not have any permissions to view cohorts.
-        return array();
-    }
-    $subqueries = array();
-    if (!empty($contextsany)) {
-        list($parentsql, $params1) = $DB->get_in_or_equal($contextsany, SQL_PARAMS_NAMED, 'ctxa');
-        $subqueries[] = 'c.contextid ' . $parentsql;
-        $params = array_merge($params, $params1);
-    }
-    if (!empty($contextsvisible)) {
-        list($parentsql, $params1) = $DB->get_in_or_equal($contextsvisible, SQL_PARAMS_NAMED, 'ctxv');
-        $subqueries[] = '(c.visible = 1 AND c.contextid ' . $parentsql. ')';
-        $params = array_merge($params, $params1);
-    }
-    $wheresql = '(' . implode(' OR ', $subqueries) . ')';
-
-    // Build the rest of the query.
-    $fromsql = "";
-    $fields = SELECT_KEYUSER_COHORT;
-    $groupbysql = '';
-    $havingsql = '';
-    if ($withmembers) {
-        $fields .= "COUNT(DISTINCT cm.userid) AS memberscnt";
-        $groupbysql = " GROUP BY c.id";
-        $fromsql = " LEFT JOIN {cohort_members} cm ON cm.cohortid = c.id ";
-        if (in_array($withmembers,
-                array(COHORT_COUNT_ENROLLED_MEMBERS, COHORT_WITH_ENROLLED_MEMBERS_ONLY, COHORT_WITH_NOTENROLLED_MEMBERS_ONLY))) {
-            list($esql, $params2) = get_enrolled_sql($currentcontext);
-            $fromsql .= " LEFT JOIN ($esql) u ON u.id = cm.userid ";
-            $params = array_merge($params2, $params);
-            $fields .= ', COUNT(DISTINCT u.id) AS enrolledcnt';
-        }
-        if ($withmembers == COHORT_WITH_MEMBERS_ONLY) {
-            $havingsql = " HAVING COUNT(DISTINCT cm.userid) > 0";
-        } else if ($withmembers == COHORT_WITH_ENROLLED_MEMBERS_ONLY) {
-            $havingsql = " HAVING COUNT(DISTINCT u.id) > 0";
-        } else if ($withmembers == COHORT_WITH_NOTENROLLED_MEMBERS_ONLY) {
-            $havingsql = " HAVING COUNT(DISTINCT cm.userid) > COUNT(DISTINCT u.id)";
-        }
-    }
-    if ($search) {
-        list($searchsql, $searchparams) = cohort_get_search_query($search);
-        $wheresql .= ' AND ' . $searchsql;
-        $params = array_merge($params, $searchparams);
-    }
-
-    $sql = $fields .
-           FROM_KEYUSER_COHORT . $fromsql .
-        " WHERE $wheresql $groupbysql $havingsql
-       ORDER BY name, idnumber";
-
-    $return = $DB->get_records_sql($sql, $params, $offset, $limit);
-    return $return;
-}
-
-// cohort_get_cohort() and cohort_get_user_cohorts() need no implementation
-// They are not called anywhere
 
 /**
  * Returns navigation controls (tabtree) to be displayed on cohort management pages
