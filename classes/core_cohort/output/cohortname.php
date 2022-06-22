2ac480d6cbaec307dd0584a68a0947d4292ee183
fbech <f.bech@koppelsberg.de>
2022-06-22 09:50:24 +0200

    2 short comments

--- classes/core_cohort/output/cohortname.php
+++ classes/core_cohort/output/cohortname.php
@@ -1,2 +1,3 @@
 <?php
+//Original class path
 require_once($CFG->dirroot.'/local/keyuser/cohort/classes/output/cohortname.php');
--- classes/tool_uploaduser/process.php
+++ classes/tool_uploaduser/process.php
@@ -1,2 +1,3 @@
 <?php
+//Original class path
 require_once($CFG->dirroot.'/local/keyuser/admin/tool/uploaduser/classes/process.php');
